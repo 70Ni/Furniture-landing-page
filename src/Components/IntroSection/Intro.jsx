558209b1bf773fc8aee8b75421b19e452c818d65
70ni <4thedevelopments@gmail.com>
2024-05-25 15:45:06 +0530

    initial commit

---
+++ src/Components/IntroSection/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div>
+        <div className="intro-wrapper">
+            <div className="intro-header">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Intro
