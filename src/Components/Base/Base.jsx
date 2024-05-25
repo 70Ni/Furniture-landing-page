4cdb214ff2381b9571fa9a144407c1343fb2d127
70ni <4thedevelopments@gmail.com>
2024-05-25 16:02:31 +0530

    Base component initial commit

---
+++ src/Components/Base/Base.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Base() {
+  return (
+    <div className="maxcontainer">
+        <div className="wrapper">
+            <img src="" alt="" />
+            <div className="text-section">
+                <div className="subHeader">We Create your home more aestetic</div>
+                <div className="para">Furnitre power is a software as services for multiperpose business management system, </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Base
