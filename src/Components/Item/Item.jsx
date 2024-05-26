0016420116c02ba1e4a6d92989831f52ed6e15a7
70ni <4thedevelopments@gmail.com>
2024-05-26 21:48:33 +0530

    Item Component

---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./item.css";
+
+import item from "../../Images/sofa.png";
+import Add from "../../Images/Add.svg";
+
+function Item() {
+  return (
+    <div className="maxcontainer">
+      <div className="wrapper">
+        <div className="item-container">
+          <div className="imgwrapper">
+            <img src={item} alt="" className="itemimage" />
+            <img src={Add} alt="" className="addIcon" />
+          </div>
+          <div className="listext">Ceiling Light</div>
+          <div className="pricing-section">
+            <div className="Navigation">$75.00</div>
+            <div className="pricing acutal-price">$75.00</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
