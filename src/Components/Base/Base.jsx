f8c73b0767fc0b4ab804c4f531f1e37a7c5b4967
70ni <4thedevelopments@gmail.com>
2024-05-26 21:32:39 +0530

    Base component

--- src/Components/Base/Base.jsx
+++ src/Components/Base/Base.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
+import React from "react";
+import "./base.css";
 
-function Base() {
+import sofa from "../../Images/sofa.png";
+import check from "../../Images/check.svg";
+
+function Base({ pos }) {
   return (
     <div className="maxcontainer">
-        <div className="wrapper">
-            <img src="" alt="" />
-            <div className="text-section">
-                <div className="subHeader">We Create your home more aestetic</div>
-                <div className="para">Furnitre power is a software as services for multiperpose business management system, </div>
+      <div
+        className="wrapper"
+        style={{ flexDirection: pos ? "row" : "row-reverse" }}
+      >
+        <img
+          src={sofa}
+          alt=""
+          className="decImg"
+          style={pos ? { marginRight: "100px" } : { marginLeft: "100px" }}
+        />
+        <div className="text-section">
+          <div className="subHeader baseHeader">
+            We Create your home more aestetic
+          </div>
+          <div className="para">
+            Furnitre power is a software as services for multiperpose business
+            management system,
+          </div>
+          {/* point warpper section */}
+          <div className="point-wrapper">
+            <div className="point-headers">
+              <img src={check} alt="" className="check-icon" />
+              <div className="button-text point-text-header">
+                Valuation Services
+              </div>
             </div>
+            <div className="Navigation point-para">
+              Sometimes features require a short description. This can be
+              detailed description
+            </div>
+          </div>
+          <div className="point-wrapper">
+            <div className="point-headers">
+              <img src={check} alt="" className="check-icon" />
+              <div className="button-text point-text-header">
+                Development of Furniture Models
+              </div>
+            </div>
+            <div className="Navigation point-para">
+              Sometimes features require a short description. This can be
+              detailed description
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Base
+export default Base;
