60d28fdc096412f66952abbaf60ff170e6e4bdd5
70ni <4thedevelopments@gmail.com>
2024-05-28 16:55:54 +0530

    List view added

---
+++ src/Components/ListView/ListView.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Item from "../Item/Item";
+import "./listview.css";
+
+function ListView() {
+  return (
+    <div className="maxcontainer">
+      <div className="wrapper">
+        <div className="List-view-wrapper">
+          <div className="list-view-header">
+            <div className="subHeader listv-header">All Products</div>
+            <div className=".Navigation listview-para">
+              The products we provide only for you as our service are selected
+              from the best products with number 1 quality in the world
+            </div>
+          </div>
+          <div className="product-list-wrapper">
+            <Item />
+            <Item />
+            <Item />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ListView;
