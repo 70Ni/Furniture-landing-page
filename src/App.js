b153d74c960952389d4561fd49ebf769d68f99c8
70ni <4thedevelopments@gmail.com>
2024-05-28 16:56:50 +0530

    App changes (test)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-let a;
-
-{
-  a = document.getElementById("fileInput");
-}
-
+import Banner from "./Components/Banner/Banner";
+import Base from "./Components/Base/Base";
+import Intro from "./Components/IntroSection/Intro";
+import Item from "./Components/Item/Item";
+import ListView from "./Components/ListView/ListView";
+import NewCategory from "./Components/Newitems/NewCategory";
+import Review from "./Components/Reviews/Review";
 
 function App() {
   return (
     <div className="App">
-   
+      <Intro />
+      <Base pos="left" />
+      <NewCategory />
+      <ListView />
+      <Base />
+      <Review />
     </div>
   );
 }
