76ccca0e289070617091592e11dac5a20e944e04
70ni <4thedevelopments@gmail.com>
2024-05-29 13:36:31 +0530

    Review wrapper responsiveness

--- src/Components/Reviews/Review.jsx
+++ src/Components/Reviews/Review.jsx
@@ -6,28 +6,42 @@ import arrowR from "../../Images/arrow-right.svg";
 
 function Review() {
   return (
-    <div className="Review-wrapper">
-      <div className="rev-profile-section">
-        <div className="rev-header-section">
-          <img src={profile} alt="" className="rev-profile" />
-          <div className="rev-pro-details">
-            <div className="button-text rev-header">Josh smith</div>
-            <div className="pricing rev-user-des">
-              Manager of The New York Times
+    <div className="maxwrapper">
+      <div className="wrapper">
+        <div className="Review-wrapper">
+          <div className="rev-profile-section">
+            <div className="rev-header-section">
+              <img src={profile} alt="" className="rev-profile" />
+              <div className="rev-pro-details">
+                <div className="button-text rev-header">Josh smith</div>
+                <div className="pricing rev-user-des">
+                  Manager of The New York Times
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="review-para para">
-          “They are have a perfect touch for make something so professional
-          ,interest and useful for a lot of people .”
-        </div>
-        <div className="review-controller-wrapper">
-          <div className="arrow-wrapper">
-            <img src={arrow} alt="next review" className="arrow" fill="#fff" />
-          </div>
-          <div className="arrow-wrapperR">
-            <img src={arrowR} alt="next review" className="arrow" fill="#fff" />
+            <div className="review-para para">
+              “They are have a perfect touch for make something so professional
+              ,interest and useful for a lot of people .”
+            </div>
+            <div className="review-controller-wrapper">
+              <div className="arrow-wrapper">
+                <img
+                  src={arrow}
+                  alt="next review"
+                  className="arrow"
+                  fill="#fff"
+                />
+              </div>
+              <div className="arrow-wrapperR">
+                <img
+                  src={arrowR}
+                  alt="next review"
+                  className="arrow"
+                  fill="#fff"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
